a31c6762c74ff116e3d96310d5f492a5bb7e96ef
ERItenov <113781299+ERItenov@users.noreply.github.com>
2022-09-17 19:31:12 +0200

    Update main.py
    Lavede ny fane om til en funktion i stedet for at kalde 3 kommandoer i samme rækkefølge to steder i koden

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Keys
 import time
+#######################################################
+#Customizables:
 
-#################
- #Customizables:
-#################
 #Hvor lang tid skal fanerne være åbne (sekunder)?
 tid = 60
 
@@ -29,32 +28,28 @@ def sus():
     driver.get('https://www.sustime.dk')
     driver.find_element(By.XPATH, '//*[@id="systime"]/div/div[2]/div/div[2]/div/div[3]/button').send_keys(Keys.RETURN)
 
+#laver og åbner ny fane, og åbner sustime i den
+def fane(n):
+    # definerer navnet på nuværende fane
+    fanenavn = "fane " + str(n)
+    # definerer kommandoen der skal køres som javascript for driveren, laver en ny tab og navngiver den
+    window_open = str("window.open('about:blank', '" + fanenavn + "'" + ');')
+    # kører javascript-kommandoen
+    driver.execute_script(str(window_open))
+    # skifter til det rigtige tab
+    driver.switch_to.window(fanenavn)
+    sus()
+
 #åbner browseren
 sus()
 #loopet til at åbne flere tabs
 #grunden til, at if else statementet for delay er udenfor funktionen er, at jeg ikke vil spilde
-#så meget som en millisekund af mit amogus tid på at checke en funktion i stedet for at amoguse
+#så meget som et millisekund af mit amogus tid på at checke en funktion i stedet for at amoguse
 if delay <= 0:
     for i in range(int(faner)):
-        #definerer navnet på nuværende fane
-        fanenavn = "fane " + str(i)
-        #definerer kommandoen der skal køres som javascript for driveren, laver en ny tab og navngiver den
-        window_open = str("window.open('about:blank', '" + fanenavn + "'"+');')
-        #kører javascript-kommandoen
-        driver.execute_script(str(window_open))
-        #skifter til det rigtige tab
-        driver.switch_to.window(fanenavn)
-        sus()
+        fane(i)
 else:
     for i in range(int(faner)):
-        # definerer navnet på nuværende fane
-        fanenavn = "fane " + str(i)
-        # definerer kommandoen der skal køres som javascript for driveren, laver en ny tab og navngiver den
-        window_open = str("window.open('about:blank', '" + fanenavn + "'" + ');')
-        # kører javascript-kommandoen
-        driver.execute_script(str(window_open))
-        # skifter til det rigtige tab
-        driver.switch_to.window(fanenavn)
-        sus()
+        fane(i)
         time.sleep(delay)
 time.sleep(tid)
