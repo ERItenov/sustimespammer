c34f22fc14200d12e174be079d48289c1ed7d374
ERItenov <113781299+ERItenov@users.noreply.github.com>
2022-09-28 13:55:06 +0200

    Update 1-gangs_ting.py

--- 1-gangs_ting.py
+++ 1-gangs_ting.py
@@ -4,3 +4,4 @@ import subprocess
 import sys
 subprocess.run([sys.executable, '-m', 'pip', 'install', '--user', 'selenium'])
 subprocess.run([sys.executable, '-m', 'pip', 'install', '--user', 'webdriver-manager'])
+subprocess.run([sys.executable, '-m', 'pip', 'install', '--user', 'pysimplegui'])
